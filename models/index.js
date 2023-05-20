0156a674c30078a038ac35194ed0185812a30127
Shiby-Mathew <shiby.ajay@gmail.com>
2023-05-20 19:54:02 +1000

    created login page,indexpage,dashboardpage

--- models/index.js
+++ models/index.js
@@ -5,25 +5,21 @@ const Comment = require("./Comment");
 //user has many Post(user_id)
 User.hasMany(Post, {
   foreignKey: "user_id",
-  onDelete: "SET NULL",
 });
 
 //user has many comments(user_id)
 User.hasMany(Comment, {
   foreignKey: "user_id",
-  onDelete: "SET NULL",
 });
 
 //Post belongs to user(user_id)
 Post.belongsTo(User, {
   foreignKey: "user_id",
-  onDelete: "SET NULL",
 });
 
 //Post has many comments(post_id)
 Post.hasMany(Comment, {
   foreignKey: "post_id",
-  onDelete: "SET NULL",
 });
 
 //comments belongs to post(post_id)
--- seeds/index.js
+++ seeds/index.js
@@ -7,11 +7,12 @@ const userSeedData = require("./user-seeds.json");
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await Post.bulkCreate(postSeedData, {
+  await User.bulkCreate(userSeedData, {
     individualHooks: true,
     returning: true,
   });
-  await User.bulkCreate(userSeedData, {
+
+  await Post.bulkCreate(postSeedData, {
     individualHooks: true,
     returning: true,
   });
